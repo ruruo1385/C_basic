73d87378369d1e409249f5d76ccba72a227d3341
Taewoong (Ricky) Um <rickyum527@gmail.com>
2026-08-15 00:30:24 +0900

    finish hw

--- redpill/day10Matrix.c
+++ redpill/day10Matrix.c
@@ -1,2 +1,42 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+void matrix_maker(int row, int col){
+    int **array;
+    //행 포인터
+    array = malloc(sizeof(int*)*row);
+    //한번에 연속되게 메모리 할당
+    array[0] = malloc(sizeof(int)*row*col);
+    //세팅
+    for (int i = 1; i < row; i++)
+    {
+        array[i] = array[0] + i * col;
+    }
+    int value = 1;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            array[i][j] = value ++;
+        }
+        
+    }
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            printf("%d ", array[i][j]);
+        }
+        printf("\n");
+    }
+    free(array[0]);
+    free(array);
+    printf(">> Memory successfully freed.\r\n");
+}
+int main(int argc, char const *argv[])
+{
+    printf("=== Day 10: Dynamic 2D Array Allocation ===\r\n");
+    printf("Generated Matrix (3x4):\r\n");
+    matrix_maker(3,4);
+    return 0;
+}
--- redpill/day8Mcpy.c
+++ redpill/day8Mcpy.c
@@ -1,2 +1,56 @@
 #include <stdio.h>
 #include <stdint.h>
+
+void *custom_memmove(void *dest, void *src, size_t n){
+    unsigned char *d = (unsigned char *)dest;
+    unsigned char *s = (unsigned char *)src;
+    if (d == s || n ==0){
+        return dest;
+    }
+    if(d<s || d>= s+ n){
+        for (size_t i = 0 ; i < n; i++){
+            d[i] = s[i];
+        }
+    }else{
+        for (size_t i = n; i > 0; i--)
+        {
+            d[i-1] = s[i-1];
+        }
+    }
+    return dest;
+}
+void printer(uint8_t *data, size_t size){
+    for (size_t i = 0; i < size; i++)
+    {
+        printf("%02x", data[i]);
+    }
+    printf("\n");
+}
+void test_data(uint8_t *data){
+    for (int i = 0; i < 10; i++)
+    {
+        data[i] = i + 1;
+    }
+}
+int main(int argc, char const *argv[])
+{
+    uint8_t data[10];
+    test_data(data);
+    printf("=== Day 8: Safe Memcpy (memmove) Implementation ===\r\n");
+    printf("[Initial] ");
+    printer(data, 10);
+    printf("\nTest 1: Overlap (Dest > Src) -> Shift Right 2 bytes\r\n");
+    custom_memmove(&data[2],&data[0], 5);
+    printf("[Result ] ");
+    printer(data,10);
+    printf(">> Success!\r\n\r\n");
+    test_data(data);
+    printf("[Initial] ");
+    printer(data, 10);
+    printf("Test 2: Overlap (Dest < Src) -> Shift Left 2 bytes\r\n");
+    custom_memmove(&data[0], &data[2],5);
+    printf("[Result ] ");
+    printer(data,10);
+    printf(">> Success!\r\n");
+    return 0;
+}
