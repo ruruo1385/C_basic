7fc655f2d5dddd44f6f8d8fa34b473bfd1e094ce
Ricky Um <rickyum527@gmail.com>
2026-08-20 15:58:13 +0900

    day13 fin

--- redpill/memPool.c
+++ redpill/memPool.c
@@ -25,13 +25,43 @@ typedef struct {
 } MemoryPool;
 
 // 1. 초기화: 모든 메모리를 쪼개서 연결 리스트로 연결
-void pool_init(MemoryPool* pool) {}
+void pool_init(MemoryPool* pool) {
+        pool->used_count = 0;
+    //풀사이즈 안에 블럭 사이즈 만큼
+    for(int i =0; i <POOL_SIZE; i++){
+        Block* curr = (Block*)&pool->memory_area[i*BLOCK_SIZE];
+        //마지막 아니면 다음블록 연결
+        if(i < POOL_SIZE - 1 ){
+            curr->next = (Block*)&pool->memory_area[(i+1)*BLOCK_SIZE];
+        }else{
+            curr -> next = NULL;
+        }
+    }
+    //가장 첫번째 블록 가르키기
+    pool->free_list = (Block*)&pool->memory_area[0];
+}
 
 // 2. 할당: Free List의 헤드(Head)를 떼어줌 (Pop)
-void* pool_alloc(MemoryPool* pool) {}
+void* pool_alloc(MemoryPool* pool) {
+    //첫번째가 없으면 널 리턴
+
+    if(pool->free_list == NULL){
+        return NULL;
+    }
+    Block* allo = pool->free_list;
+    //첫번째 포인터에 다음거를 설정
+    pool->free_list = pool->free_list->next;
+    pool->used_count ++;
+    return(allo);
+}
 
 // 3. 해제: 반환된 블록을 Free List의 헤드에 다시 붙임 (Push)
-void pool_free(MemoryPool* pool, void* ptr) {}
+void pool_free(MemoryPool* pool, void* ptr) {
+    Block* block = (Block*)ptr;
+    block ->next = pool->free_list;
+    pool->free_list = block;
+    pool->used_count --;
+}
 
 // 테스트 코드
 int main() {
@@ -42,7 +72,7 @@ int main() {
     void* p1 = pool_alloc(&my_pool);
     void* p2 = pool_alloc(&my_pool);
     void* p3 = pool_alloc(&my_pool);
-
+    printf("[Init] Memory Pool Initialized (10 blocks of 32 bytes)\r\n");
     printf("Allocated: %p, %p, %p\n", p1, p2, p3);
     printf("Used Blocks: %d\n", my_pool.used_count);
 
