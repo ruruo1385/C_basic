52313bf9ab02e06f28542b1c7ebade65f49757e2
Ricky Um <rickyum527@gmail.com>
2026-08-26 16:46:15 +0900

    still working

--- redpill/day17.c
+++ redpill/day17.c
@@ -4,15 +4,33 @@
 #define SIZE 2
 #define TOTAL (SIZE * 8)
 uint8_t bitmap[SIZE] = {0};
-void bitmapAlloc(int index){
+int checkAlloc(int index){
     int byte = index / 8;
     int bit = index % 8;
     return (bitmap[byte] >> bit) & 1;
+}
+void setBit(int index){
+    int byte = index / 8;
+    int bit = index % 8;
+    bitmap[byte] |= (1<<bit);
+    printf(">> Index %d allocated. (Byte %d, Bit %d set)\n",
+           index, byte, bit);
+}
+void bitmapAlloc(int index){
+    for(int i =0; i < TOTAL; i++){
+        if(!checkAlloc(i)){
+            setBit(i);
+            return i;
+        }
+    }
+    return -1;
 }
 void bitmapFree(int index){
     int byte = index / 8;
     int bit = index % 8;
     bitmap[byte] &= ~(1<<bit);
+    printf(">> Index %d freed. (Byte %d, Bit %d cleared)\n",
+           index, byte, bit);
 }
 int main(int argc, char const *argv[])
 {
