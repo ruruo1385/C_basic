aa8611845b024634fc3846bda082c530b106ce30
Ricky Um <rickyum527@gmail.com>
2026-08-28 10:29:50 +0900

    Initial Start of HW + day 19 stack overflow

---
+++ redpill/day19.c
@@ -0,0 +1,27 @@
+#include <stdint.h>
+#include <stdio.h>
+#define STACK_LIMIT 4096
+
+char *stack_start;
+void recursive(int depth){
+    char local;
+    if(depth == 0){
+        stack_start = &local;
+    }
+    size_t usage = (size_t)(stack_start - &local);
+    printf("Current Stack Usage: %zu bytes\n",usage);
+    if(usage > STACK_LIMIT){
+        printf("Soft Limit Exceeded\n");
+        return;
+    }
+    recursive(depth + 1);
+}
+
+int main(int argc, char const *argv[])
+{
+    printf("Soft Limit: %d bytes\n",STACK_LIMIT);
+    recursive(0);
+    /* code */
+    return 0;
+}
+
